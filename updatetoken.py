d47f06d43485757af1a3c3377f7241f9ef1f8a07
SoDebug <30922923+SoDebug@users.noreply.github.com>
2025-02-05 17:29:04 +0800

    Update updatetoken.py: debug

--- updatetoken.py
+++ updatetoken.py
@@ -24,7 +24,9 @@ def gettoken(refresh_token):
           'redirect_uri':'http://localhost:53682/'
          }
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
+    printf(str(html))
     jsontxt = json.loads(html.text)
+    printf(jsontext)
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
     with open(path, 'w+') as f:
